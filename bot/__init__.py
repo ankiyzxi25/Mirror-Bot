c8a9e74278fc237f07734d645157d62a2de7ee71
ankiyzxi25 <74183863+ankiyzxi25@users.noreply.github.com>
2021-06-13 12:05:36 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -84,7 +84,7 @@ except:
 try:
     BOT_TOKEN = getConfig("BOT_TOKEN")
     parent_id = getConfig("GDRIVE_FOLDER_ID")
-    DOWNLOAD_DIR = "/mnt/sdc/Mirror-Bot/downloads/
+    DOWNLOAD_DIR = getConfig("DOWNLOAD_DIR")
     if not DOWNLOAD_DIR.endswith("/"):
         DOWNLOAD_DIR += "/"
     DOWNLOAD_STATUS_UPDATE_INTERVAL = int(getConfig("DOWNLOAD_STATUS_UPDATE_INTERVAL"))
